fb5ed2a445e0a790e2c3137a85517149c6634b6c
tkay <aruntknambiar@gmail.com>
2016-03-08 19:02:11 +0530

    removed local app version constrints

--- plugin/init_compiler.js
+++ plugin/init_compiler.js
@@ -90,7 +90,7 @@ function _packageJsContent () {
   function deps(api) {
 
 
-    var d = ["underscore","mwc:compiler@1.1.43","mwc:extensions@1.0.22"];
+    var d = ["underscore","mwc:compiler","mwc:extensions"];
     var mwcFilePath = path.resolve('client/compiler.mwc.json');
     if (mwcFilePath) {
       var mwcFile = JSON.parse(fs.readFileSync(mwcFilePath, 'utf8'));
